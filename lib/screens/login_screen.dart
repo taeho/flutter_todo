8cf3f381e3c9744c7c834ed39de126fa936a5761
taehokang <gopae02@gmail.com>
2022-12-11 15:26:37 +0900

    sample login 기능 구현

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:flutter_todo/screens/list_screen.dart';
 
 class LoginScreen extends StatelessWidget {
+  Future setLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isLogin', true);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('LoginScreen')),
+      appBar: AppBar(
+        title: Text('로그인'),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          width: MediaQuery.of(context).size.width * 0.85,
+          child: ElevatedButton(
+            onPressed: () {
+              setLogin().then((_) {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => ListScreen())
+                );
+              });
+            },
+            child: Text('로그인'),
+          ),
+        ),
+      ),
     );
   }
 }
