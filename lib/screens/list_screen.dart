0c48d6d3c023524e4267af678152ccf5dc5e698f
taehokang <gopae02@gmail.com>
2022-12-11 16:35:22 +0900

    todo 추가, 조회 추가

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,10 +1,146 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class ListScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/models/todo.dart';
+import 'package:flutter_todo/providers/todo_default.dart';
+
+class ListScreen extends StatefulWidget {
+  @override
+  _ListScreenState createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  List<Todo> todos = [];
+  TodoDefault todoDefault = TodoDefault();
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      todos = todoDefault.getTodos();
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('ListScreen')),
+      appBar: AppBar(
+        title: Text('할 일 목록 앱'),
+        actions: [
+          InkWell(
+            onTap: () {},
+            child: Container(
+              padding: EdgeInsets.all(5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.book),
+                  Text('News'),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('+', style: TextStyle(fontSize: 25)),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                String title = '';
+                String description = '';
+                return AlertDialog(
+                  title: Text('할 일 추가하기'),
+                  content: Container(
+                    height: 200,
+                    child: Column(
+                      children: [
+                        TextField(
+                          onChanged: (value) {
+                            title = value;
+                          },
+                          decoration: InputDecoration(labelText: '제목'),
+                        ),
+                        TextField(
+                          onChanged: (value) {
+                            description = value;
+                          },
+                          decoration: InputDecoration(labelText: '설명'),
+                        )
+                      ],
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                        child: Text('추가'),
+                        onPressed: () {
+                          setState(() {
+                            print("[UI] ADD");
+                            todoDefault.addTodo(
+                              Todo(title: title, description: description),
+                            );
+                          });
+                          Navigator.of(context).pop();
+                        }),
+                    TextButton(
+                        child: Text('취소'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        }),
+                  ],
+                );
+              });
+        },
+      ),
+      body: isLoading ? Center(
+        child: CircularProgressIndicator(),
+      )
+      : ListView.separated(itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(todos[index].title),
+          onTap: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return SimpleDialog(
+                    title: Text('할 일'),
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text('제목 : ' + todos[index].title),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text('설명 : ' + todos[index].description),
+                      ),
+                    ],
+                  );
+                });
+          },
+          trailing: Container(
+            width: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  child: InkWell(
+                    child: Icon(Icons.delete),
+                    onTap: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }, separatorBuilder: (context, index) {
+        return Divider();
+      }, itemCount: todos.length),
     );
   }
 }
