aaed2550ea72296dcf73dd6b5306856cedfa386c
TaeHo Kang <taehokang@TaeHoui-MacBookPro.local>
2022-12-11 15:14:11 +0900

    Initial commit

---
+++ lib/screens/list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('ListScreen')),
+    );
+  }
+}
