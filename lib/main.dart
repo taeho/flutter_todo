5a480a74492cb218a37c63e0b742eea9aed891bb
taehokang <gopae02@gmail.com>
2023-01-15 14:36:51 +0900

    todo firestore 로 기능 수정 #2 news api 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/screens/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+//void main() {
+//  runApp(const MyApp());
+//}
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
